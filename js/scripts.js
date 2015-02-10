975f74cac9a13b5f1bc592db8c9ffc7e91b0412f
Amy Michelle Johnson & Sarah Joy Calpo <student@epicodus.com>
2015-02-09 16:26:19 -0800

    Refactor scripts.js using regular expression for a DRYier code.

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,9 @@ var pigLatin = function(sentence) {
   var pigword_array = []
   sentence.split(" ").forEach(function(word){
     var word = word.toLowerCase();
-    var vowels = "aeiou"
-    var first_letter = word[0]
-    var second_letter = word[1]
-    var third_letter = word[2]
-
-    if (vowels.indexOf(first_letter)!==-1) {
-      var pigword = word.concat("ay");
-      pigword_array.push(pigword)
-    } else if (first_letter === "q") {
-      var pigword = word.slice(2).concat(word[0]).concat(word[1]).concat("ay");
-      pigword_array.push(pigword)
-    } else if (second_letter === "q") {
-      var pigword = word.slice(3).concat(word[0]).concat(word[1]).concat(word[2]).concat("ay");
-      pigword_array.push(pigword)
-    } else {
-
-      if (vowels.indexOf(second_letter) !==-1) {
-        var pigword= word.slice(1).concat(word[0]).concat("ay");
-        pigword_array.push(pigword)
-      } else {
-        if (vowels.indexOf(third_letter) !==-1) {
-          var pigword = word.slice(2).concat(word[0]).concat(word[1]).concat("ay");
-          pigword_array.push(pigword)
-
-        } else {
-          var pigword = word.slice(3).concat(word[0]).concat(word[1]).concat(word[2]).concat("ay");
-          pigword_array.push(pigword)
-
-        };
-      };
-    };
+    var me = /^([squ]+|[y]|[^aieouy]+)(\w*)/ ;
+    var pigword = (word.replace(me, "$2$1")).concat("ay");
+    pigword_array.push(pigword) ;
   });
   return pigword_array.join(" ")
 };
@@ -41,13 +13,8 @@ $(function() {
   $("form#pig-latin").submit(function(event) {
     var english = $("input#english").val();
     var pigword = pigLatin(english);
-
     $("#translation").show();
-
     $("#piglatin").text(pigword);
-
     event.preventDefault();
   });
-
-
 });
